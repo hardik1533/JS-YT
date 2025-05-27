d8cad5ab321241fa19761d8beb7a6d83162d0cf1
hardik1533 <123451135+hardik1533@users.noreply.github.com>
2025-05-27 15:42:20 +0000

    01 VARIABLES

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -1,7 +1,7 @@
 const Password = "12345"
-var userId = "1533"
-let userName = "Hardik"
-countryName = "India"
+var userId = "1533"  //old method
+let userName = "Hardik" //new method (scope problem)
+countryName = "India" //you can write without variable name but it's not best way
 
 // Password = "15331533"  // you cannot change const Value
 
