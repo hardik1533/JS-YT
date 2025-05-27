41bc292d7282f7f1745948c50a23e03d1cda485b
hardik1533 <123451135+hardik1533@users.noreply.github.com>
2025-05-27 15:36:17 +0000

    Variables Sikh liye 1st Lec

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,11 @@
+const Password = "12345"
+var userId = "1533"
+let userName = "Hardik"
+countryName = "India"
+
+// Password = "15331533"  // you cannot change const Value
+
+console.log(Password)
+
+console.table([Password,userId,userName,countryName])
+
