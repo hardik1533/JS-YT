f8688fc147c8721206be1343835b829667ddc5ba
hardik1533 <123451135+hardik1533@users.noreply.github.com>
2025-05-28 11:44:06 +0000

    02 DataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+let age = 19; //number
+
+let age_2 = 1000000000000000000000000000000000000000000000000000000000000000000000000000000000000; //Bigint
+
+let userName = "Hardik"; //string
+
+let isTrue = false; //Boolean
+
+let value = null; //Data Type of null is an object
+
+let value_2; //Data Type of an undefined is undefined
+
+
+
+//Symbol
+
+// Object
+
+
+console.table([typeof age, typeof age_2, typeof userName, typeof isTrue, typeof value, typeof value_2]);
